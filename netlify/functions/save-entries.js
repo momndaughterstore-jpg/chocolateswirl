071110520e32a547f2b4be1b8554b1ea243f415c
momndaughterstore-jpg <momndaughterstore@gmail.com>
2026-05-09 14:54:09 +0100

    Create save-entries.js

---
+++ netlify/functions/save-entries.js
@@ -0,0 +1,20 @@
+const { getStore } = require('@netlify/blobs');
+
+exports.handler = async (event) => {
+  if (event.httpMethod !== 'POST') {
+    return { statusCode: 405, body: 'Method not allowed' };
+  }
+  try {
+    const store = getStore({ name: 'journal', consistency: 'strong' });
+    await store.set('entries', event.body);
+    return {
+      statusCode: 200,
+      body: JSON.stringify({ ok: true })
+    };
+  } catch (err) {
+    return {
+      statusCode: 500,
+      body: JSON.stringify({ error: err.message })
+    };
+  }
+};
